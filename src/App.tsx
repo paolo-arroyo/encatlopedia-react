d69c2f67a9473121eaba4291ccf154fadaab0d8e
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-07-02 15:21:49 +0800

    Add Routes & Pages (home, breed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home'
+import Breed from './pages/breed'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Breed />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
