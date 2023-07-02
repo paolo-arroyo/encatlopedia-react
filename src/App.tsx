647b7a1bab2b11f84017bb01f1ce3a408c1bd61f
paolo-arroyo <89679296+paolo-arroyo@users.noreply.github.com>
2023-07-02 19:46:10 +0800

    Add effect hook to get cat breeds

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
 import Breed from './pages/breed'
 
+// Type Declarations (Breed Data, Cat Images)
+export type BreedType = {
+  id: string,
+  name: string,
+  temperament: string,
+  origin: string,
+  country_code: string,
+  life_span: string,
+  description: string,
+  indoor: boolean,
+  wikipedia_url: string,
+  weight: {
+      imperial: string,
+      metric: string
+  },
+  scores: {
+      adaptability: number,
+      affection_level: number,
+      child_friendly: number,
+      dog_friendly: number,
+      stranger_friendly: number,
+      energy_level: number,
+      grooming: number,
+      health_issues: number,
+      intelligence: number,
+      shedding_level: number,
+      social_needs: number,
+  }
+}
+
+
 const App = () => {
   return (
     <div className="App">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { Card, Form } from 'react-bootstrap'
+import BreedType from '../../App'
+import axios from 'axios'
+
+//API Constants
+const BREEDS_URL = "https://api.thecatapi.com/v1/breeds/"
+
+// Type Declarations (Breed Data, Cat Images)
+export type BreedType = {
+    id: string,
+    name: string,
+    temperament: string,
+    origin: string,
+    country_code: string,
+    life_span: string,
+    description: string,
+    indoor: boolean,
+    wikipedia_url: string,
+    weight: {
+        imperial: string,
+        metric: string
+    },
+    scores: {
+        adaptability: number,
+        affection_level: number,
+        child_friendly: number,
+        dog_friendly: number,
+        stranger_friendly: number,
+        energy_level: number,
+        grooming: number,
+        health_issues: number,
+        intelligence: number,
+        shedding_level: number,
+        social_needs: number,
+    }
+  }
 
 const Home = () => {
+    const [ breeds, setBreeds ] = useState<BreedType[]>([])
+
+    const BreedHook = () => {
+        try {
+            axios.get(BREEDS_URL).then(response => {
+                setBreeds(response.data)
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(BreedHook, [])
+    console.log(breeds)
   return (
-    <div>Home</div>
+    <div>
+        <h1> Encatlopedia </h1>
+        <Form.Select aria-label="Select Cat Breed">
+            <option> Select Breed </option>
+        </Form.Select>
+    </div>
   )
 }
 
